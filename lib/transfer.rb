e566779b64291e8df7882f0098b3cbf1e484f237
Brittany Morris <brittmmendez@gmail.com>
2017-12-18 19:17:10 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
 
   def valid?
-     (@sender.valid? and @receiver.valid?) and @status== "pending"
+     (@sender.valid? and(((@sender.balance)-@amount)>=0) and @receiver.valid?) and @status== "pending"
 
   end
 
