caa1d813b6c126964fe0168501d77eeb44afa996
Brittany Morris <brittmmendez@gmail.com>
2017-12-18 18:39:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
 
-  attr_accessor :status
+  attr_accessor :status, :receiver, :sender, "amount"
 
   def initialize(sender,receiver,amount)
     @sender=sender
     @receiver=receiver
-    @status="pending"
+    @amount=amount
+    @status= "pending"
   end
 
 end
